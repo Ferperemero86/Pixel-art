350345da2ba40b02eaff4af52c970f5eab853d2a
Ferperemero86 <fernandoperezmerono@gmail.com>
2018-01-16 01:07:35 +0000

    "Javascript work done"

--- designs.js
+++ designs.js
@@ -12,25 +12,38 @@ const canvasSize = document.querySelector('#pixel_canvas');
 
 nameValue.addEventListener('submit',function(evt){
   evt.preventDefault();
+  document.querySelector('table').remove();
   let canvasHeight = document.getElementById('input_height').value;
   let canvasWidth = document.getElementById('input_width').value;
 
   function makeGrid(canvasHeight,canvasWidth){
+     const myTable = document.createElement('TABLE');
+      myTable.setAttribute('id','pixel_canvas');
+     document.body.append(myTable);
+
     for(let r=1;r<=parseInt(canvasHeight);r++){
      let trCanvas = document.createElement('tr');
-      canvasSize.appendChild(trCanvas);
+      myTable.append(trCanvas);
       for(let d=1;d<=parseInt(canvasWidth);d++){
      let tdCanvas = document.createElement('td');
-      trCanvas.appendChild(tdCanvas);
+      trCanvas.append(tdCanvas);
       }
     }
+
+    document.getElementById("colorPicker").addEventListener('change',function(evt){
+      let color = evt.target.value;
+      document.querySelector('#pixel_canvas').addEventListener('click', function (evt) {
+      if (evt.target.nodeName === 'TD') {
+         evt.target.style.backgroundColor = color;
+       }
+      });
+    });
+
   }
 
   makeGrid(canvasHeight,canvasWidth);
 });
 
-colorValue.addEventListener('change',function(){
-  console.log(colorValue.value);
-});
+
 
 });// End of  loading DOM
