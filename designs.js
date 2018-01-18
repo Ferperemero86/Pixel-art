e71a703c0b9997e050f2dddbf9527d5d0dd90aff
Ferperemero86 <fernandoperezmerono@gmail.com>
2018-01-18 22:42:27 +0000

    Improve javascript styles

--- designs.js
+++ designs.js
@@ -1,8 +1,8 @@
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
 
 // Select color input
-const colorValue = document.getElementById("colorPicker");
+const colorValue = document.getElementById('colorPicker');
 
 // Select size input
 const gridSizeForm = document.getElementById('sizePicker');
@@ -10,27 +10,27 @@ const canvasSize = document.querySelector('#pixel_canvas');
 
 // When size is submitted by the user, call makeGrid()
 
-gridSizeForm.addEventListener('submit',function(evt){
+gridSizeForm.addEventListener('submit',function(evt) {
   evt.preventDefault();
   document.querySelector('table').remove();
   let canvasHeight = document.getElementById('input_height').value;
   let canvasWidth = document.getElementById('input_width').value;
 
-  function makeGrid(canvasHeight,canvasWidth){
+  function makeGrid(canvasHeight,canvasWidth) {
      const myTable = document.createElement('TABLE');
-      myTable.setAttribute('id','pixel_canvas');
+     myTable.setAttribute('id','pixel_canvas');
      document.body.append(myTable);
 
-    for(let r=1;r<=parseInt(canvasHeight);r++){
+    for(let r = 1; r <= parseInt(canvasHeight); r++) {
      const trCanvas = document.createElement('tr');
-      myTable.append(trCanvas);
-      for(let d=1;d<=parseInt(canvasWidth);d++){
-     const tdCanvas = document.createElement('td');
-      trCanvas.append(tdCanvas);
+     myTable.append(trCanvas);
+      for(let d = 1; d <= parseInt(canvasWidth); d++) {
+       const tdCanvas = document.createElement('td');
+       trCanvas.append(tdCanvas);
       }
     }
 
-    document.getElementById("colorPicker").addEventListener('change',function(evt){
+    document.getElementById('colorPicker').addEventListener('change',function(evt) {
       let color = evt.target.value;
       document.querySelector('#pixel_canvas').addEventListener('click', function (evt) {
       if (evt.target.nodeName === 'TD') {
